e1316501f60350d2d359583c553e8fc758d2cd33
Emmi Raffo <emmyraffoc@gmail.com>
2021-07-20 11:42:59 -0400

    Se agregó primera iteración de CSS

---
+++ src/configFirebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHTyqk4ajz0njIfMyDR9SZNLeLe_FYM4c",
+    authDomain: "mira-4b75e.firebaseapp.com",
+    projectId: "mira-4b75e",
+    storageBucket: "mira-4b75e.appspot.com",
+    messagingSenderId: "92599889665",
+    appId: "1:92599889665:web:a248d5b78ad8f66303f500",
+    measurementId: "G-LQKNJXGMZS"
+  };
+
+
+  export firebaseConfig ;git 
