270df50c42780aeed16f9c74a7236b6426618940
Emmi Raffo <emmyraffoc@gmail.com>
2021-07-21 20:42:22 -0400

    Carpetas de rutas

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import  auth  from './lib/auth.js';
 
 // para registrarse
 document.getElementById("crearCuentaUsuario").addEventListener("click", () => {
-
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
@@ -15,18 +14,29 @@ document.getElementById("signIn").addEventListener("click", () =>{
     let email = document.getElementById("email2").value;
     let password = document.getElementById("password2").value;
 
-    auth.autEmailPass(email, password)
+    auth.autEmailPass(email, password, () => { 
+        gestionarSecciones("sectionOne","sectionTwo")
+    })
 });
 
 // para ingreso con Google
 
 document.getElementById("authGoogle").addEventListener("click", () =>{
- auth.authCuentaGoogle()
+ auth.authCuentaGoogle(() => { 
+     gestionarSecciones("sectionOne","sectionTwo")
+ })
+ 
 })
-
-
 
 // para cerrar sesión
 document.getElementById("signOut").addEventListener("click", () =>{
     auth.signOut();
 });
+
+//Para gestionar SPA
+
+function gestionarSecciones(ocultar, mostrar) {
+document.getElementById(ocultar).style.display="none";
+document.getElementById(mostrar).style.display="block";
+
+}
